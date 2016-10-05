c5fe8bd4c00488c8c4eb9275b61f8282c5cfa9ef
Vitaly Sastre <vitalysastre@gmail.com>
2016-10-05 20:46:21 +0200

    Rectificacion de codigo.

--- src/Practica7_RefuerzoInterfaz/Student.java
+++ src/Practica7_RefuerzoInterfaz/Student.java
@@ -4,7 +4,31 @@ package Practica7_RefuerzoInterfaz;
  * Created by vitaly94 on 05/10/2016.
  */
 public class Student {
-    public Student() {
-        //...
+    private String dni;
+    private String fechaNacimiento;
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
     }
 }
